1b134b5d0981b154f4d018447f00ef69615bf849
MishManners <mishmanners@github.com>
2020-10-15 15:31:17 +1100

    Update LIFX-TV-scene.py

--- Old-Scripts/LIFX-TV-scene.py
+++ Old-Scripts/LIFX-TV-scene.py
@@ -2,6 +2,6 @@
 # Get Scene UUID Here: https://api.developer.lifx.com/docs/activate-scene
 import pifx
 
-p = pifx.PIFX(api_key='cf63ed4685e0d94bc6d4c617e6716d9dc8a3978847c9fe434f805b7330233ab1')
+p = pifx.PIFX(api_key='add API key here')
 
-p.activate_scene(scene_uuid='583c65b0-1a53-4632-9bdd-5497e801e127', duration=0.2)
+p.activate_scene(scene_uuid='add scene key here', duration=0.2)
